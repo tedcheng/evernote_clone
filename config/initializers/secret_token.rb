152d01964224e1cc04d9afcdcff4abd9e8d6a2ce
Ted Cheng <yiqun.cheng@gmail.com>
2013-09-02 11:51:25 -0700

    initial commit: start of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evernote::Application.config.secret_token = '6767dfc41680d23522528ba945984e6138cbf8cc9d747b6553ebd8205255658f5b26faf11db25f9a8c3c4c28413da8481a0c32557bb8e2abb05283d0bb2ded5b'
